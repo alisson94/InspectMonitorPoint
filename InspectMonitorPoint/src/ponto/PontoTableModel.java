a62a1b66911fd1ad502727849a6cc70eba42a5bf
TIEverton <evertonpinheiroti@hotmail.com>
2019-09-19 16:03:27 -0300

    Classe Ponto

---
+++ InspectMonitorPoint/src/ponto/PontoTableModel.java
@@ -0,0 +1,72 @@
+package ponto;
+
+import aluno.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class PontoTableModel extends AbstractTableModel {
+
+    private List<Aluno> alunos = new ArrayList<>();
+    private String[] colunas = {"ID", "Matrícula", "Nome", "E-mail", "CPF", "Telefone"};
+
+    public PontoTableModel(List<Aluno> usuarios) {
+        this.alunos = usuarios;
+    }
+
+    @Override
+    public int getRowCount() {
+        return alunos.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Aluno aluno = alunos.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return aluno.getId();
+
+            case 1:
+                return aluno.getMatricula();
+
+            case 2:
+                return aluno.getNome();
+
+            case 3:
+                return aluno.getEmail();
+
+            case 4:
+                return aluno.getCpf();
+
+            case 5:
+                return aluno.getTelefone();
+
+        }
+        return null;
+    }
+
+    public String getColumnName(int index) {
+        switch (index) {
+            case 0:
+                return colunas[0];
+            case 1:
+                return colunas[1];
+            case 2:
+                return colunas[2];
+            case 3:
+                return colunas[3];
+            case 4:
+                return colunas[4];
+            case 5: 
+                return colunas[5];
+
+        }
+        return null;
+    }
+
+}
