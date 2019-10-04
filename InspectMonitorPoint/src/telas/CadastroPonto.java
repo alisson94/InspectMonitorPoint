20ee3159ec94257f56cd5d668a41ea6ff860ec17
wellingtonsatBR <54953170+wellingtonsatBR@users.noreply.github.com>
2019-10-04 09:52:50 -0300

    Correção dos campos
    Quais são os campos obrigatorios e os opcionais.

--- InspectMonitorPoint/src/telas/CadastroPonto.java
+++ InspectMonitorPoint/src/telas/CadastroPonto.java
@@ -293,6 +293,7 @@ public class CadastroPonto extends javax.swing.JFrame {
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
